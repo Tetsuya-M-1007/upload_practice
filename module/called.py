a0b0cd900d44aff28dc70e3c50b3c966c116fbed
Tetsuya-M-1007 <63894177+Tetsuya-M-1007@users.noreply.github.com>
2022-05-06 19:07:01 +0900

    Add files via upload

---
+++ module/called.py
@@ -0,0 +1,8 @@
+class myFunc:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        out = self.x + self.y
+        return out
---
+++ test.py
@@ -0,0 +1,4 @@
+from module.called import myFunc
+
+cls = myFunc(5, 10)
+print(cls.add())
